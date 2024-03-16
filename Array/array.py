19a212bc161f789dbffe553ff3fa521713695715
SFJT-97 <santiagojuarez.dev@gmail.com>
2024-03-16 11:07:41 -0400

    Array!

---
+++ Array/array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+words1 = []
+words2 = np.array["World!", "just", "started."]
+
+words1.extend(["Hello", "I'm", "getting"])
+
+print(words1[0], words2[0], words1[1], words2[1], words1[2], words2[2])
